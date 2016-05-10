40c45715cd60319589ce3d489422ae69359c4b2e
MariaLapovska <maria.lapovsky@gmail.com>
2016-05-11 01:23:56 +0300

    Update Controller.java

--- src/com/epam/lab2/var20/Controller.java
+++ src/com/epam/lab2/var20/Controller.java
@@ -7,12 +7,7 @@ public class Controller {
 	Model model;
 	View view;
 	
-	/**
-	 * Constructor with parameters
-	 * 
-	 * @param model Model object
-	 * @param view View object
-	 */
+	/* Constructor with parameters */
 	public Controller(Model model, View view) {
 		this.model = model;
 		this.view = view;
@@ -37,36 +32,34 @@ public class Controller {
 	 * @param scanner Input scanner to read from.
 	 * @return Four digits positive integer value from input.
 	 */
-    public int inputFourDigitIntValueWithScanner(Scanner scanner) {
-    	String str;
+    	public int inputFourDigitIntValueWithScanner(Scanner scanner) {
+    		String str;
     	
-        while (true) {
-        	str = scanner.next();
-        	
-        	if (!checkNumber(str)) {
-        		view.printMessage(View.WRONG_INPUT_INT_DATA);
-        		view.printMessage(View.INPUT_INT_DATA);
-        	} else { //string has correct format
-        		break;
-        	}
-        }
+        	while (true) {
+	        	str = scanner.next();
+	        	
+	        	if (!checkNumber(str)) {
+	        		view.printMessage(View.WRONG_INPUT_INT_DATA);
+	        		view.printMessage(View.INPUT_INT_DATA);
+	        	} else { //string has correct format
+	        		break;
+	        	}
+	        }
 
-        return Integer.valueOf(str);
-    }
-    
-    /**
-     * Checks if the given string contains 4 digit integer value.
-     * 
-     * @param string String to check.
-     * @return
-     * true if string contains 4 digit integer value, 
-     * false otherwise.
-     */
-    public boolean checkNumber(String string) {
-    	if (string.matches("[1-9][0-9]{3}")) { // "1023"
-    		return true;
-    	} else {
-    		return false;
+        	return Integer.valueOf(str);
     	}
-    }
+    
+	/**
+	* Checks if the given string contains 4 digit integer value.
+	* 
+	* @param string String to check.
+	* @return Boolean.
+	*/
+	public boolean checkNumber(String string) {
+		if (string.matches("[1-9][0-9]{3}")) { // "1023"
+		    return true;
+		} else {
+		    return false;
+		}
+	}
 }
